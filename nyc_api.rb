41b566ffcfb21801fdf62a6b1e29e0152e5247c0
Andrew Hinojosa <drewsview34@outlook.com>
2020-07-26 22:43:19 +0000

    Automatically backed up by Learn

--- nyc_api.rb
+++ nyc_api.rb
@@ -1,2 +1,18 @@
-answer = "flamboyance"
-puts "A group of flamingos is called a #{answer}."
+require 'net/http'
+require 'open-uri'
+require 'json'
+ 
+class GetPrograms
+ 
+  URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+ 
+  def get_programs
+    uri = URI.parse(URL)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
+ 
+end
+ 
+programs = GetPrograms.new.get_programs
+puts programs
